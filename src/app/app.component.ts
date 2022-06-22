7a01e5907b11933010bdcafcb3dfa8c64ff967f8
Reshetnik-Eugenius <Eugenius@example.com>
2022-06-22 10:16:45 +0300

    current block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherService } from './shared/service/weather.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ export class AppComponent {
   public isVisibleCurrent = true;
   public isVisibleForecast = false;
   cityName: string = 'Minsk, Belarus';
+  today: any;
+  weeks: any;
+
+  constructor(private weatherService: WeatherService) {
+    this.getInputValue();
+    // console.log('app-constructor - '+ this.city);
+    // this.getDataToday();
+    // this.getData5Days();
+}
 
   btnClickToday(){
     console.log("button click today");
@@ -22,6 +32,23 @@ export class AppComponent {
   }
   getInputValue():string{
     console.log(this.cityName);
+    this.getDataCurrent();
+    this.getDataForecast();
+
     return this.cityName;
   }
+
+  getDataCurrent():void {
+    this.weatherService.getWeatherForCurrentDay(this.cityName).subscribe((data: any) => {
+        this.today = data;
+        console.log(this.today);
+    })
+}
+
+getDataForecast():void {
+    this.weatherService.getWeatherFor5Days(this.cityName).subscribe((data: any) => {
+        this.weeks = data.list;
+        console.log(this.weeks);
+    })
+}
 }
--- src/app/components/current/current.component.ts
+++ src/app/components/current/current.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentComponent implements OnInit {
 
+  @Input() today: any;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  durationDay(value_sunset:number, value_sunrise:number):string{
+    let miliseconds:number = value_sunset - value_sunrise;
+    let hour:any = Math.round(miliseconds / 60 / 60) % 24;
+    hour = hour > 0 ? hour-1: hour;
+    let min:any = Math.round(miliseconds / 60 % 60);
+
+    min = min < 10 ? '0' + min : min;
+    hour = hour < 10 ? '0' + hour : hour;
+
+    return (hour+':'+min+' '+'hr');
+}
 }
